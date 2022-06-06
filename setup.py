849c57bfd3257475b5edc12e5882e0e4710e683a
Menglong.Li <albert.menglongli@gmail.com>
2022-06-06 22:54:48 +0800

    Version 1.1.27 add eq funs for all classes in dict_utils, add filter_allowable

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '1.1.26'
+version = '1.1.27'
 
 long_description = 'A toolbox' \
                    ' containing multi useful pythonic utils, functions, decorators etc.'
