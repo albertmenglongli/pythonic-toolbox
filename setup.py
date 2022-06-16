88e3e92f383235fdbc125658e924936e85645e65
Menglong.Li <albert.menglongli@gmail.com>
2022-06-16 16:51:52 +0800

    Version 1.1.29 unpack_list supports more data types

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '1.1.28'
+version = '1.1.29'
 
 long_description = 'A toolbox' \
                    ' containing multi useful pythonic utils, functions, decorators etc.'
