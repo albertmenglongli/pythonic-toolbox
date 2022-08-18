699e8540576cfae3d673344133904212fc3a5da8
Menglong.Li <albert.menglongli@gmail.com>
2022-08-18 16:29:11 +0800

    Add Supported python versions badge

--- tests/generate_readme_markdown.py
+++ tests/generate_readme_markdown.py
@@ -56,6 +56,7 @@ pip3 install pythonic-toolbox --upgrade
 BADGES = (f"""
 [![PyPI version](https://badge.fury.io/py/pythonic-toolbox.svg)](https://badge.fury.io/py/pythonic-toolbox)
 [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+[![Supported Python versions](https://img.shields.io/pypi/pyversions/pythonic-toolbox.svg?style=flat&logo=python&logoColor=yellow&labelColor=5c5c5c)](https://pypi.org/project/pythonic-toolbox)
 [![CodeQL Status]({BADGE_CODEQL_LINK})]({WORKFLOW_CODEQL_LINK})
 [![Python3.6 Test Status]({BADGE_TEST_PY36_LINK})]({WORKFLOW_TEST_PY36_LINK})
 [![Python3.7 Test Status]({BADGE_TEST_PY37_LINK})]({WORKFLOW_TEST_PY37_LINK})
